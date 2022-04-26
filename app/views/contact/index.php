f77752ae08659e9d496a76b35fa31e6aa0ab64ac
Oleksandr <zadorozhniy3@icloud.com>
2022-04-26 15:55:55 +0300

    redit contact

--- app/views/contact/index.php
+++ app/views/contact/index.php
@@ -15,7 +15,7 @@
                     <p>Мій email: <a href="mailto:admin@alexproger.com">admin@alexproger.com</a></p><br>
                     <hr>
                     <p>BackEnd розробник!</p>
-                    <p>Мій диплом: <a href="https://goo.su/bFOZ">посилання</a></p></br>
+                    <p>Мій диплом: <a href="https://goo.su/bFOZ" target="_blank">посилання</a></p></br>
                     <p>Мої работи:</p>
                     <ul>
                         <li>завантажити: <a href="/public/files/Articles.zip" download>"Статті"</a></li>
@@ -24,7 +24,7 @@
                     </ul><br>
                     <hr>
                     <p>FrontEnd розробник!</p>
-                    <p>Мій диплом: <a href="#">(вивчаю)</a></p></br>
+                    <p>Мій диплом: <a href="https://goo.su/HIc2V8" target="_blank">посилання</a></p></br>
                     <p>Мої работи:</p>
                     <ul>
                         <li>завантажити: <a href="/public/files/Music.zip" download>"Music"</a></li>
